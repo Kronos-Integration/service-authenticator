70d2baa343036090dafb8529689e1afac69547bb
Markus Felten <Markus.Felten@gmx.de>
2019-12-13 18:18:59 +0100

    test: wip

--- tests/auth-source.mjs
+++ tests/auth-source.mjs
@@ -1,27 +1,24 @@
 import { Service } from "@kronos-integration/service";
 
-
 export class AuthSource extends Service {
+  static get endpoints() {
+    return {
+      ...super.endpoints,
+      authenticate: {
+        receive: "authenticate"
+      }
+    };
+  }
 
-    static get endpoints() {
-        return {
-            ...super.endpoints,
-            authenticate: {
-                default: true,
-                receive: "authenticate"
-            }
-        };
+  async authenticate(credentials) {
+    const { username, password } = credentials;
+
+    if (password !== "test") {
+      throw new Error("invalid credentials");
     }
-
-    async authenticate(credentials) {
-        const { username, password } = credentials;
-
-        if (password !== "test") {
-            throw new Error('invalid credentials');
-        }
-        const response = { username, entitlements: new Set(['a', 'b']) };
-        return response;
-    }
+    const response = { username, entitlements: new Set(["a", "b"]) };
+    return response;
+  }
 }
 
 export default AuthSource;
--- tests/auth-test.mjs
+++ tests/auth-test.mjs
@@ -1,4 +1,4 @@
-import test from "ava"
+import test from "ava";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
@@ -10,34 +10,39 @@ import { ServiceAuthenticator } from "../src/service-authenticator.mjs";
 const here = dirname(fileURLToPath(import.meta.url));
 
 const config = {
-    auth: {
-        type: ServiceAuthenticator,
-        endpoints: {
-            ldap: "service(ldap).authenticate"
-        },
-        jwt: {
-            public: readFileSync(join(here, "fixtures", "demo.rsa.pub")),
-            private: readFileSync(join(here, "fixtures", "demo.rsa"))
-        }
+  auth: {
+    type: ServiceAuthenticator,
+    endpoints: {
+      ldap: "service(ldap).authenticate"
     },
-    ldap: {
-        type: AuthSource
+    jwt: {
+      public: readFileSync(join(here, "fixtures", "demo.rsa.pub")),
+      private: readFileSync(join(here, "fixtures", "demo.rsa"))
     }
+  },
+  ldap: {
+    type: AuthSource
+  }
 };
 
 test("service-auth", async t => {
-    const sp = new StandaloneServiceProvider();
-    const [auth] = await sp.declareServices(config);
-    await auth.start();
+  const sp = new StandaloneServiceProvider();
+  const [auth] = await sp.declareServices(config);
+  await auth.start();
 
-    t.is(auth.state, "running");
+  t.log(auth.endpoints.access_token);
+  t.true(auth.endpoints.access_token.isConnected(sp.services.ldap.endpoints.authenticate));
+  t.is(auth.state, "running");
 
-    const response = await auth.endpoints.access_token.receive({ username: "user1", password: "test" });
+  const response = await auth.endpoints.access_token.receive({
+    username: "user1",
+    password: "test"
+  });
 
-   // t.is(response.username, 'user1');
-    const access_token = response.access_token;
-    const data = JSON.parse(Buffer.from(access_token.split(".")[1], "base64"));
+  // t.is(response.username, 'user1');
+  const access_token = response.access_token;
+  const data = JSON.parse(Buffer.from(access_token.split(".")[1], "base64"));
 
-    console.log(data);
-    t.deepEqual(data.entitlements.split(/,/), ["a", "b"]);
+  console.log(data);
+  t.deepEqual(data.entitlements.split(/,/), ["a", "b"]);
 });
